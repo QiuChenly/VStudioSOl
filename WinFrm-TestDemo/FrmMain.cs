78b9caabf765522f7684c0dec044310f915c416f
玉奇 陈 <chenyuqi2466535763@outlook.com>
2017-02-03 13:57:52 +0800

    添加项目文件。

---
+++ WinFrm-TestDemo/FrmMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFrm_TestDemo
+{
+    public partial class FrmMain : Form
+    {
+        httphelper web = new httphelper();
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMe_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WinFrm-TestDemo/httphelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using MSScriptControl;
+using System.Text;
+
+public class httphelper
+{
+    /// <summary>
+    /// 取出中间文本
+    /// </summary>
+    /// <param name="alltext">全部文本</param>
+    /// <param name="lefttext"></param>
+    /// <param name="righttext"></param>
+    /// <returns>返回取到的文本</returns>
+    public string BetweenText
+        (string alltext, string lefttext, string righttext)
+    {
+        int index = alltext.IndexOf(lefttext,0)+lefttext.Length;
+        return alltext.Substring(index, alltext.IndexOf(righttext, index) - index);
+    }
+    public string JavaScriptEval(string CurrentDir, string EvalStr)
+    {
+        ScriptControl script = new ScriptControl();
+        StreamReader str = new StreamReader(CurrentDir, Encoding.Unicode);
+        string stri = str.ReadToEnd();
+        str.Close();
+        str.Dispose();
+        script.Language = "JavaScript";
+
+
+    }
+
+}
