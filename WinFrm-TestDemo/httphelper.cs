302f38ea18b0586cc265d0c748bf74ebc7b0ec75
玉奇 陈 <chenyuqi2466535763@outlook.com>
2017-02-03 14:18:23 +0800

    修正JS计算函数

--- WinFrm-TestDemo/httphelper.cs
+++ WinFrm-TestDemo/httphelper.cs
@@ -16,9 +16,15 @@ public class httphelper
     public string BetweenText
         (string alltext, string lefttext, string righttext)
     {
-        int index = alltext.IndexOf(lefttext,0)+lefttext.Length;
+        int index = alltext.IndexOf(lefttext, 0) + lefttext.Length;
         return alltext.Substring(index, alltext.IndexOf(righttext, index) - index);
     }
+    /// <summary>
+    /// JS计算
+    /// </summary>
+    /// <param name="CurrentDir">js文件地址</param>
+    /// <param name="EvalStr">计算公式 例如 "getpwd('pwd')"</param>
+    /// <returns></returns>
     public string JavaScriptEval(string CurrentDir, string EvalStr)
     {
         ScriptControl script = new ScriptControl();
@@ -27,8 +33,9 @@ public class httphelper
         str.Close();
         str.Dispose();
         script.Language = "JavaScript";
-
-
+        script.AddCode(stri);
+        stri = script.Eval(EvalStr);
+        return stri;
     }
 
 }
