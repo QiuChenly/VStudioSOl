37086acd28aca3a84f2bd1b2301b09501d8ff940
玉奇 陈 <chenyuqi2466535763@outlook.com>
2017-02-04 18:05:52 +0800

    创建bilibili登录工程

---
+++ bilibili网站登录/FrmMain.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace bilibili网站登录
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.txtpwd = new System.Windows.Forms.TextBox();
+            this.txtverify = new System.Windows.Forms.TextBox();
+            this.picverify = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picverify)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnlogin.Location = new System.Drawing.Point(117, 187);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(75, 23);
+            this.btnlogin.TabIndex = 0;
+            this.btnlogin.Text = "登录";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "账号";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "密码";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "验证码";
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(48, 15);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(229, 21);
+            this.txtuser.TabIndex = 4;
+            // 
+            // txtpwd
+            // 
+            this.txtpwd.Location = new System.Drawing.Point(48, 44);
+            this.txtpwd.Name = "txtpwd";
+            this.txtpwd.Size = new System.Drawing.Size(229, 21);
+            this.txtpwd.TabIndex = 5;
+            // 
+            // txtverify
+            // 
+            this.txtverify.Location = new System.Drawing.Point(109, 128);
+            this.txtverify.Name = "txtverify";
+            this.txtverify.Size = new System.Drawing.Size(92, 21);
+            this.txtverify.TabIndex = 6;
+            // 
+            // picverify
+            // 
+            this.picverify.Location = new System.Drawing.Point(60, 72);
+            this.picverify.Name = "picverify";
+            this.picverify.Size = new System.Drawing.Size(200, 50);
+            this.picverify.TabIndex = 7;
+            this.picverify.TabStop = false;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 233);
+            this.Controls.Add(this.picverify);
+            this.Controls.Add(this.txtverify);
+            this.Controls.Add(this.txtpwd);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnlogin);
+            this.Name = "FrmMain";
+            this.Text = "bilibili登录";
+            ((System.ComponentModel.ISupportInitialize)(this.picverify)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.TextBox txtpwd;
+        private System.Windows.Forms.TextBox txtverify;
+        private System.Windows.Forms.PictureBox picverify;
+    }
+}
+
